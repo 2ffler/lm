134eb908fd279fd0ef83ffcfcb1bf52f2d0b680d
2ffler <83592582+2ffler@users.noreply.github.com>
2021-09-02 13:37:28 +0900

    Update imageCreate.js

--- docs/js/datacreate/imageCreate.js
+++ docs/js/datacreate/imageCreate.js
@@ -1,11 +1,16 @@
 var btn = document.getElementById("create");
 
 btn.addEventListener("click",() => {
-	html2canvas(document.getElementById("previeW")).then(canvas => { 
-	    let downloadEle = document.getElementById("download");
+	html2canvas(document.getElementById("previeW")).then(canvas => {
+		result = document.getElementById("result");
+		itis = document.getElementById("canvas");
+		download = document.getElementById("download");
 
-	    donwloadEle.href = canvas.toDataURL();
-	    donloadEle.download = "canvas.png";
-	    downloadEle.click();
+		itis.remove()
+
+		canvas.id = 'canvas'
+		download.style = 'display: block;'
+
+	    result.appendChild(canvas)
 	});
 })
