1697ff2e928dbfabfde32926a57a2a77f0fcc630
2ffler <83592582+2ffler@users.noreply.github.com>
2021-09-02 11:44:31 +0900

    Update mesadd.js

--- docs/js/datacreate/mesadd.js
+++ docs/js/datacreate/mesadd.js
@@ -1,102 +1,286 @@
-var adm = 1;
+var adm = 0;
+
+function imageCreateThis(image, nameing) {
+	const file = document.querySelector(nameing).files[0];
+	var reader = new FileReader()
+
+	if (file){
+		reader.readAsDataURL(file);
+	} else {
+		image.innerHTML = '<img src="./image/nonamethis.png">'
+	};
+
+	// ファイル読み取りに失敗したとき
+	reader.onerror = function() {
+		image.remove();
+	}
+	// ファイル読み取りに成功したとき
+	reader.onload = function() {
+		image.innerHTML = `<img src="${reader.result}">`
+	};
+};
 
 function addmessage(obj) {
 	behind = document.getElementById('preView');
+	design = document.getElementById('design');
 
-	behind.style = "display: none;";
+	backgroundcolor = design.elements['backgroundcolor'].value
+	kidokuColor = '#000000'
 
-	check1data = document.getElementById("addD");
+	if (backgroundcolor == '#1A1A1A') {
+		kidokuColor = '#ffffff'
+	};
+
+	if (behind){
+		behind.remove()
+	};
+
+	checkmemberCount = document.getElementById("delb").value;
+
+	check1data = document.querySelector("input[name='addD']").checked;
 	check3type = document.querySelector("select[name='type']").value;
 
 	talk = document.createElement('div');
-	talk.id = `talk_${adm}`;
 
-	if (check3type == 0) {
-		return;
-	} else if (check1data == true){
+	var textmessage = document.createElement('div');
+
+	if (check1data == true || check3type == 4){
 		//日付
-		check11 = document.querySelector("div[name='mdata']").value;
-		talk.class = 'chuou';
+		check11 = document.getElementById("mdatainput").checked;
+		talk.className = 'chuoublock';
+		textmessage.className = 'chuou'
+		textmessage.style = `color: ${kidokuColor}`
 
-		if (check11 == 0) {
+		if (check3type == 4 && check1data == false){
+			//メッセージの送信取消
+			check2sender = document.querySelector("select[name='sender']").value;
+
+			textmessage.innerHTML = ''
+
+			if (check2sender > 0){
+				senderName = document.getElementById(`sender_${check2sender}`).value;
+				textmessage.innerHTML = senderName + 'が'
+			};
+
+			textmessage.innerHTML += 'メッセージの送信を取り消しました。';
+		} else if (check11 == true) {
 			check12 = document.getElementById("data").value;
-			talk.innerHTML = `${check12}`;
-			talk.class = 'chuou';
+			check11 = document.querySelector("input[name='year']").checked;
+
+			date = check12.split('-')
+
+			check12 = ''
+
+			if (check11 == true) {
+				check12 = date[0] + '年'
+			};
+
+			check12 += date[1] + '月' + date[2] + '日'
+
+			textmessage.innerHTML = `${check12}`;
 		} else {
-			talk.class = 'chuou';
-			talk.innerHTML = `${check11}`;
+			check11 = document.querySelector("input[name='mdata']").value;
+			check12 = document.getElementById("yesterday").checked;
+
+			if (check12 == true) {
+				check11 = '昨日'
+			};
+
+			textmessage.innerHTML = `${check11}`;
 		};
+		talk.appendChild(textmessage)
 
-	} else if (check3type == 4){
-		//メッセージの送信取消
-		check2sender = document.querySelector("select[name='sender']").value;
-		talk.class = 'chuou';
-		talk.innerHTML = `${check2sender}がメッセージの送信を取り消しました。`;
+		delmes = 'システムメッセ：' + textmessage.innerHTML
 
+	} else if (check3type == 0) {
+		return
 	} else {
 		//既読時間つくやつ
 		check2sender = document.querySelector("select[name='sender']").value;
 
-		if (check2sender == 0){
-			talk.class = 'rComment';
-		} else {
-			talk.class = 'cComment';
-		};
+		senderName = document.getElementById(`sender_${check2sender}`).value;
+		myName = document.querySelector('input[id="green"]').checked;
 
-		check3type = document.querySelector("select[name='type']").value;
-
-		if (check3type == 5){
-			//イラスト
-			imagethis = document.querySelector("input[name='image']");
-
-			const fileData = imagethis.target.files[0];
-
-			const imgType = fileData.type;
-
-			var reader = new FileReader();
-			// ファイル読み取りに失敗したとき
-			reader.onerror = function() {
-				alert('読み取り失敗！')
-			}
-			// ファイル読み取りに成功したとき
-			reader.onload = function() {
-				const insert = '<img src=' + reader.result + 'class="backPreview">';
-				if (check2sender == 0){
-					talk.class = 'rComment';
-				} else {
-					talk.class = 'cComment';
-				};
-				talk.innerHTML = insert;
-			}
-			// ファイル読み取りを実行
-			reader.readAsDataURL(fileData);
-		} else {
-			if (check3type == 1){
-				textmessage = document.querySelector("textarea[name='message']").value;
-			} else if (check3type == 2){
-				var textmessage = document.createElement('div');
-				textmessage.class = "tell";
-				textmessage.innerHTML = '(';
-			} else if (check3type == 3){
-				telltime = document.getElementById("tuwatimeopen").value;
-				var textmessage = document.createElement('div');
-				textmessage.innerHTML = '<div class="tell">(</div>' + telltime;
+		if (senderName == '') {
+			if (check2sender == 0){
+				senderName = '黄緑の方'
+			} else {
+				senderName = `${check2sender}人目の話相手`
 			};
 		};
 
-		check4time = document.getElementById("timehyouji").value;
-		check5kidoku = document.getElementById("kidoku").value;
+		var txtContents = document.createElement('div');
+		var txtP = document.createElement('p')
+		var sankaku = document.createElement('div')
+		var icon = document.createElement('div')
 
-		if (check5kidoku == true){
-			check51 = '既読';
+		if (check2sender == 0){
+			talk.className = 'rComment';
+			which = 'right'
+			border = 'left'
+
+			fontcolor = design.elements['myfontcolor'].value
+			mesbackcolor = design.elements['mycolor'].value
+		} else {
+			talk.className = 'cComment';
+			which = 'left'
+			border = 'right'
+
+			fontcolor = design.elements['fontcolor'].value
+			mesbackcolor = design.elements['mescolor'].value
 		};
+
+
+		if (which == 'left') {
+			talk.appendChild(icon)
+			imageCreateThis(icon, `input[id='icon_${check2sender}']`)
+		};
+
+		textmessage.className = `${which}Chatting`
+		txtContents.className = `${which}Says`
+		txtP.className = `${which}font`
+		sankaku.className = `${which}3kaku`
+		icon.className = `${which}Faceicon`
+
+		check3type = document.querySelector("select[name='type']").value;
+
+		delmes = '送信者：' + senderName + '　メッセ内容：'
+
+		senderNameopenthis = document.getElementById('senderNameopen').checked;
+
+		if (senderNameopenthis && senderName != '黄緑の方'){
+			senderNameContent = document.createElement('div')
+
+			senderNameContent.className = 'sendername'
+			senderNameContent.innerHTML = '<div class="kidoku">' + senderName + '</div>'
+
+			textmessage.appendChild(senderNameContent)
+		};
+
+		if (check3type == 5){
+			//イラスト
+
+			imageCreateThis(txtContents, "input[id='mesImage']")
+
+			delmes += '＜画像＞'
+
+		} else {
+
+			txtP.innerHTML = document.querySelector("textarea[name='message']").value;
+
+			txtContents.style = `background-color: ${mesbackcolor};`
+			sankaku.style = `border-${border}-color: ${mesbackcolor};`
+			txtP.style = `color: ${fontcolor};`
+
+			if (check3type == 2 || check3type == 3){
+				txtP.innerHTML = '<div class="telln"><div class="tell">)</div></div>';
+
+				if (check3type == 3){
+					telltime = document.querySelector("input[name='tuwatime'").value;
+					timeThis = telltime.split(':')
+
+					if (timeThis[0] == '00'){
+						telltime = timeThis[1] + ':' + timeThis[2]
+					};
+					delmes += '＜通話履歴＞' + telltime
+				} else if (check2sender == 0) {
+					delmes += '＜応答なし＞'
+					telltime = '応答なし'
+				} else {
+					delmes += '＜不在着信＞'
+					telltime = '不在着信'
+				};
+
+				txtP.innerHTML += '<div style="text-align: center;">' + telltime + '</div>';
+			} else {
+				delmes += '「' + txtP.innerHTML + '」'
+			};
+
+			txtContents.appendChild(sankaku)
+			txtContents.appendChild(txtP)
+		};
+
+		check4time = document.getElementById("timehyouji").checked;
+		check5kidoku = document.getElementById("kidoku").checked;
+
+		check51 = ''
+
+		if (check5kidoku == true && check2sender == 0){
+			check51 = '既読'
+			if (checkmemberCount > 1){
+				kidokuCount = document.querySelector("select[name='kidokuCount']").value;
+				check51 += ` ${kidokuCount}`
+			};
+		};
+
 		if (check4time == true){
-			check41 = document.querySelector("select[name='time']").value;
+			check41 = document.querySelector("input[name='time']").value;
 			check51 += `<br>${check41}`
 		};
 
-		talkdisplay = document.getElementById('talkadd');
-		talkdisplay.appendChild(talk);
-		textmessage.appendChild(talkdisplay);
+		kidokuTime = document.createElement('div')
+		kidokuTime.className = 'kidoku'
+		kidokuTime.innerHTML = check51
+
+		if (which == 'right'){
+			textmessage.appendChild(kidokuTime);
+		};
+
+		textmessage.appendChild(txtContents)
+
+		if (which == 'left'){
+			textmessage.appendChild(kidokuTime);
+		};
+
+		talk.appendChild(textmessage);
+
+		if (which == 'right' && myName == true) {
+			talk.appendChild(icon)
+			imageCreateThis(icon, `icon_${check2sender}`)
+		};
 	};
+
+	adm += 1
+	talk.id = `talk_${adm}`;
+
+	talkdisplay = document.getElementById('talkadd');
+
+	add = document.querySelector("select[name='mespos']");
+
+	if (add.value == 'new'){
+		talkdisplay.appendChild(talk);
+	} else {
+		beforeContents = document.getElementById(add.value)
+		talkdisplay.insertBefore(talk, beforeContents)
+	}
+
+	del = document.querySelector("select[name='meslist']");
+
+	childthis = document.createElement('option');
+
+	childthis.value = talk.id
+	childthis.innerHTML = delmes + 'の前に挿入'
+
+	if (add.value == 'new'){
+		beforeContents = document.querySelector("option[value='new']");
+	} else {
+		beforeContents = document.querySelector(`option[value='${add.value}']`);
+	};
+
+	add.insertBefore(childthis, beforeContents)
+
+	childthat = document.createElement('option');
+
+	childthat.value = talk.id + 'd'
+	childthat.innerHTML = delmes
+
+	if (add.value == 'new'){
+		del.appendChild(childthat)
+	} else {
+		beforeContents = document.querySelector(`option[value='${add.value}d']`);
+		del.insertBefore(childthat, beforeContents)
+	};
+
+	button = document.getElementById('create');
+	button.style = 'display: block;'
 };
