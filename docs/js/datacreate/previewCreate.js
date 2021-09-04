cce5ee3b1cdd17062da5f34ebab1c8aefaba1375
2ffler <83592582+2ffler@users.noreply.github.com>
2021-09-04 10:54:37 +0900

    Update previewCreate.js

--- docs/js/datacreate/previewCreate.js
+++ docs/js/datacreate/previewCreate.js
@@ -49,39 +49,5 @@ if(window.File) {
 		// ファイル読み取りを実行
 		reader.readAsDataURL(fileData);
 
-//		var img = new Image();
-
-//		img.onload = function() {
-			//トリミング
-//				if (img.width * 16 < img.height * 9){
-//					var H = img.width / 9 * 16;
-//					var H0 = (img.height - H) / 2;
-//					var HM = img.height - H0 * 2;
-//					var W0 = 0;
-//					var WM = img.width;
-//				} else {
-//					var W = img.height * 9 / 16;
-//					var H = 0;
-//					var W0 = (img.width - W) / 2;
-//					var WM = img.width - W0 * 2;
-//					var H0 = 0;
-//					var HM = img.height;
-//				}
-//				var ImgCanvas = document.getElementById("canVas");
-//
-//				ImgCanvas.width = 750;
-//				ImgCanvas.height = 1334;
-//
-//				var ImgCanvasCtx = ImgCanvas.getContext("2d");
-//
-//				ImgCanvasCtx.clearRect(0,0,ImgCanvas.width,ImgCanvas.height);
-//				ImgCanvasCtx.drawImage(img,W0,H0,WM,HM,0,0,750,1334);
-//
-//				ImgCanvasCtx.fillStyle = "black";
-//				ImgCanvasCtx.globalAlpha = 0.4;
-//				ImgCanvasCtx.font = '400 24px "M PLUS 1p", sans-serif';
-//				ImgCanvasCtx.fillText("LIER", 90, ih);
-//			};
-//		img.src = reader.result;
 	}, false);		
 };
