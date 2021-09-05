8d0f95825e856d44745d69c2d133f3f4ced24413
2ffler <83592582+2ffler@users.noreply.github.com>
2021-09-05 20:53:31 +0900

    Update imageCreate.js

--- docs/js/datacreate/imageCreate.js
+++ docs/js/datacreate/imageCreate.js
@@ -3,7 +3,7 @@ var btn = document.getElementById("result");
 btn.addEventListener("click",() => {
 
 	html2canvas(document.getElementById("zoom")).then(canvas => {
-		createImage = document.getElementById("createImage")
+		zooming = document.getElementById("zooming");
 		that = document.getElementById("canvas");
 		onemore = document.getElementById("thisDownload");
 		download = document.getElementById("download");
@@ -15,9 +15,10 @@ btn.addEventListener("click",() => {
 		download.style = 'display: block;'
 		onemore.style.display = 'block'
 
-		createImage.appendChild(canvas)
+		zooming.appendChild(canvas)
 
 		var link = canvas.toDataURL("image/jpeg");
 		document.getElementById("download").href = link;
+		document.getElementById("resultImg").href = link;
 	});
 })
